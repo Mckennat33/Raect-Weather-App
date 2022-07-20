293744e22f198884ae65cbb5cf3fef9b287489ae
Thomas <thomasmckenna12gmail.com>
2022-07-19 19:17:17 -0500

    added tailwind + icons

---
+++ react-weather-app/src/components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function SearchBar() {
+
+const cities = [
+    {
+        id: 1, 
+        title: "Chicago"
+    },
+    {
+        id: 1, 
+        title: "New York"
+    },
+    {
+        id: 1, 
+        title: "Seattle"
+    },
+    {
+        id: 1, 
+        title: "Austin"
+    },
+    {
+        id: 1, 
+        title: "Dallas"
+    }
+
+]
+
+    return (
+    <div>
+        <h1>Searchbar</h1>
+    </div>
+    )
+}
+
+export default SearchBar
